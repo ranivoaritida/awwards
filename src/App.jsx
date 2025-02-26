2b06a2093e2156bbb618e188fa67a03683d528aa
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2025-02-26 11:02:43 +0300

    project skeleton

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-500 font-bold' >Welcome to the awards</h1>
+    </main>
+  )
+}
+
+export default App
