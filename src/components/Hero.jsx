2fecf630668f094e8b2f7a9ad21d936f1a19860c
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2025-03-04 14:02:04 +0300

    hero css correction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 
 import { useRef, useState } from 'react';
 const Hero = () => {
-    const [currentIndex, setCurrentIndex] = useState(1);
+    const [currentIndex, setCurrentIndex] = useState(0);
     const [hasClicked, setHasClicked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
@@ -24,7 +24,7 @@ const Hero = () => {
     <div className="relative h-dvh w-screen overflow-x-hidden">
         <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
             <div>
-                <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+                <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg zentry">
                     <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
                         <video ref={nextVideoRef} src={getVideoSrc(currentIndex + 1)} loop muted id="current-video" className='size-64 origin-center scale-150 object-cover object-center'
                             onLoadedData={handleVideoLoad}
