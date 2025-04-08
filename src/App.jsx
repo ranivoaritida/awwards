c91f22951a9ddde0add9a6a6679599a76d4bde50
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2025-04-08 12:12:48 +0300

    about begin animatedTtle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About';
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
-      <section className="bg-blue-500 min-h-screen" />
+      <About />
     </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,6 +20,7 @@ const Hero = () => {
 
     const handleVideoLoad = () => {
         setLoadedVideos( (prev) => prev + 1);
+        console.log(loadedVideos);
     }
 
     const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
@@ -79,7 +80,7 @@ const Hero = () => {
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
     
   return (
-    <div className="relative h-dvh w-screen ">
+    <div className="relative h-dvh w-screen overflow-x-hidden ">
         {isLoading && (
             <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-500">
                 <div className='three-body'>
@@ -89,7 +90,7 @@ const Hero = () => {
                 </div>
             </div>
         )}
-        <div id="video-frame" className="relative z-10 h-dvh w-screen  rounded-lg bg-blue-75">
+        <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
             <div>
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg zentry">
                     <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
