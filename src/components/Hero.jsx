c2d5bdce9bda1c9af3c387500193389136679707
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2025-04-08 10:27:48 +0300

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,21 @@
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Button from './Button';
 import gsap from "gsap";
-import { TiLocationArrow } from 'react-icons/ti';
 import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/all";
+import { TiLocationArrow } from 'react-icons/ti';
+
+
+gsap.registerPlugin(ScrollTrigger);
+
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 3;
+    const totalVideos = 4;
     const nextVideoRef = useRef(null);
 
     const handleVideoLoad = () => {
@@ -23,6 +28,12 @@ const Hero = () => {
         setHasClicked(true);
         setCurrentIndex(upcomingVideoIndex);
     }
+
+   useEffect(() => {
+        if(loadedVideos === totalVideos - 1){
+            setIsLoading(false);
+        }
+    },[loadedVideos])
 
     useGSAP (()=> {
         if(hasClicked){
@@ -44,15 +55,41 @@ const Hero = () => {
                 duration: 1.5,
                 ease: 'power1.inOut',
             })
-            
-
         }
     },{dependencies: [currentIndex],revertOnUpdate: true})
+
+    useGSAP(() => {
+        gsap.set('#video-frame',{
+            clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+            borderRadius: '0% 0% 40% 10%',
+        })
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            borderRadius: '0% 0% 0% 0%',
+            ease: 'power1.inOut',
+            scrollTrigger : {
+                trigger: '#video-frame',//when the animation should happen
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        })
+    })
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
     
   return (
-    <div className="relative h-dvh w-screen overflow-x-hidden">
-        <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
+    <div className="relative h-dvh w-screen ">
+        {isLoading && (
+            <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-500">
+                <div className='three-body'>
+                    <div className='three-body__dot'/>
+                    <div className='three-body__dot'/>
+                    <div className='three-body__dot'/>
+                </div>
+            </div>
+        )}
+        <div id="video-frame" className="relative z-10 h-dvh w-screen  rounded-lg bg-blue-75">
             <div>
                 <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg zentry">
                     <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'>
@@ -67,7 +104,7 @@ const Hero = () => {
                     ref={nextVideoRef}
                     src={getVideoSrc(currentIndex)}
                     loop
-                    muted
+                    //muted
                     id="next-video"
                     className='absolute-center invisible absolute z-20 size-64 object-cover object-center '
                     onLoadedData={handleVideoLoad}
@@ -99,6 +136,6 @@ const Hero = () => {
         </h1>
     </div>
   )
-}
+};
 
 export default Hero
