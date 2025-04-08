a1c0db5e2cf0cdaa70976ea0b8cc8e48591e0cd6
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2025-04-08 15:41:54 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 
+import { useRef } from 'react';
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
 const Navbar = () => {
+    const navContainerRef = useRef(null);
   return (
-    <div>Navbar</div>
+    <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-0 transition-all duration-700 sm:inset-x-6' >
+        <header className="absolute top-1/2 w-full -translate-y-1/2">
+            <nav className='flex size-full items-center justify-between p-4'>
+                <div className='flex items-center gap-7'>
+                    <img src='/img/logo.png' alt="logo" className='w-10' />
+                    <Button
+                        id='product-button'
+                        title="Products"
+                        rightIcon={<TiLocationArrow />}
+                        containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+                    />
+                </div>
+                <div className='flex h-full items-center'>
+                    <div className="hidden md:block"></div>
+                </div>
+            </nav>
+        </header>
+    </div>
   )
 }
 
